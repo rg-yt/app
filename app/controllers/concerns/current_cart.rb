58ebab75475849b6a49529b20016c70c18180c28
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-12-06 15:17:07 -0500

    Fix issues with current cart session and add tests

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -4,7 +4,7 @@ module CurrentCart
   def set_cart
     @cart = Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
-    @card = Cart.create
-    session[:card_id] = @cart.id
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
   end
 end
