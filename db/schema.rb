ac2930b68ed7e0617b78b269b6e3bca9729a9761
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-12-06 13:45:14 -0500

    Generate cart scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_03_033118) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_06_183831) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
