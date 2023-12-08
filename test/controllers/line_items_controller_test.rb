a44f161e8280d2685d25a0ab254cdab4ab344e15
rg-yt <103154533+rg-yt@users.noreply.github.com>
2023-12-07 23:05:34 -0500

    Finish G iteration

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -20,8 +20,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       post line_items_url, params: { product_id: products(:ruby).id }
     end
     follow_redirect!
-    assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', "1 \u00D7 Programming Ruby 1.9"
+    assert_select 'h2', 'Your Cart'
+    assert_select 'td', 'Programming Ruby 1.9'
   end
 
   test 'should show line_item' do
